5520a4563ebbfc6436f4d425fe99e43e07c04867
timothé - renaud langlois - teston <timothe8833@gmail.com>
2020-11-09 16:03:15 +0100

    modified + add some comment

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
  */
 
 const puppeteer = require('puppeteer');
-//const iPhone = puppeteer.devices['iPhone 6'];
+const iPhone = puppeteer.devices['iPhone 6'];
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -25,14 +25,11 @@ const puppeteer = require('puppeteer');
   d.setMinutes(d.getMinutes() + 5);
   var TimeZer = d.getHours() +" "+ d.getMinutes();
 
-
+ //Ordres fonctionnel sur la pages web des montagnes
   const page = await browser.newPage();
-  //await page.emulate(iPhone);
-
-  // Suite des ordres des montagnes
-  await page.goto('https://media.interieur.gouv.fr/deplacement-covid-19/');  
-  
-  await page.keyboard.press('Enter');
+  await page.emulate(iPhone);
+  await page.goto('https://media.interieur.gouv.fr/deplacement-covid-19/');  //u can change th URL 
+  await page.keyboard.press('Enter'); //dismiss the update popup that displayed on the page (normaly)
   await page.type('#field-firstname', 'First-Name');
   await page.type('#field-lastname', 'LastName');
   await page.type('#field-birthday', '01011998');
@@ -43,15 +40,12 @@ const puppeteer = require('puppeteer');
   await page.type('#field-datesortie', Jour);
 
   //Issue with date time here
-  await page.type('#field-heuresortie', TimeZer);
+  await page.type('#field-heuresortie', TimeZer); 
   await page.click('#checkbox-travail');
+  
+  //validation du Form
   await page.click('#generate-btn');
   await page.keyboard.press('Enter');
-  /* 
-  await page.keyboard.press('Enter');
-  await page.keyboard.press('Enter');
-  await page.keyboard.press('Enter');
-  await page.keyboard.press('Enter'); */
   
 
   // Output File 
