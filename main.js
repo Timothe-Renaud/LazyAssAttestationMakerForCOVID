58ebaef0d3e1909039fab3344cd62cf7dd1ca472
timothé - renaud langlois - teston <timothe8833@gmail.com>
2020-11-10 10:47:39 +0100

    c push sec

--- main.js
+++ main.js
@@ -6,11 +6,11 @@
  * - npm i pupperteer
  * - relancé l'IDE
  * - lancé le script vie l'IDE
+ * 
  * Le script fonctionne a 70% voir les autres branches pour les issues.
  */
 
 const puppeteer = require('puppeteer');
-const iPhone = puppeteer.devices['iPhone 6'];
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -23,32 +23,27 @@ const iPhone = puppeteer.devices['iPhone 6'];
   var Jour = today.getDay() +" "+ today.getMonth() +" "+ today.getFullYear();
   var d = today; 
   d.setMinutes(d.getMinutes() + 5);
-  var TimeZer = d.getHours() +" "+ d.getMinutes();
+  var TimeZer = d.getHours() +""+ d.getMinutes();
 
  //Ordres fonctionnel sur la pages web des montagnes
   const page = await browser.newPage();
-  await page.emulate(iPhone);
-  await page.goto('https://media.interieur.gouv.fr/deplacement-covid-19/');  //u can change th URL 
+  await page.goto('https://media.interieur.gouv.fr/deplacement-covid-19/');  //u can change the URL 
   await page.keyboard.press('Enter'); //dismiss the update popup that displayed on the page (normaly)
-  await page.type('#field-firstname', 'First-Name');
-  await page.type('#field-lastname', 'LastName');
-  await page.type('#field-birthday', '01011998');
-  await page.type('#field-placeofbirth', 'MaVilleZer');
-  await page.type('#field-address', 'Mon adresseZer');
-  await page.type('#field-city', 'MaVille');
-  await page.type('#field-zipcode', 'MyZip');
-  await page.type('#field-datesortie', Jour);
+  await page.keyboard.press('Enter');
+  await page.type('#field-firstname', 'First-Name', {delay: 10});
+  await page.type('#field-lastname', 'LastName', {delay: 10});
+  await page.type('#field-birthday', '01011998', {delay: 10});
+  await page.type('#field-placeofbirth', 'MaVilleZer', {delay: 5});
+  await page.type('#field-address', 'Mon adresseZer', {delay: 9});
+  await page.type('#field-city', 'MaVille', {delay: 7});
+  await page.type('#field-zipcode', '94110', {delay: 3});
+  await page.type('#field-datesortie', Jour, {delay: 4});
 
   //Issue with date time here
-  await page.type('#field-heuresortie', TimeZer); 
+  await page.click('#field-heuresortie', TimeZer, {delay:3});
+  await page.keyboard.press('Enter'); 
   await page.click('#checkbox-travail');
-  
+
   //validation du Form
   await page.click('#generate-btn');
-  await page.keyboard.press('Enter');
-  
-
-  // Output File 
-  await page.pdf({path: 'page.pdf'});
-})
-();
+})();
