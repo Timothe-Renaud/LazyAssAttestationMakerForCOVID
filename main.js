ac9a9ca3762d29c08c8c692adc5eec4e53e0fb36
timothé - renaud langlois - teston <timothe8833@gmail.com>
2020-11-10 14:02:59 +0100

    la saisie est bonne/rapide/la date est bien avancé de 5 min, bref IZ GUUUT

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 /**
  * @author: Timothé-Renaud Langlois - Teston
  * @date: 09/11/2020
- * Genere automatiquement ton certificat de sortis COVID
- * Procedur:
+ * Génere automatiquement ton certificat de sortis COVID
+ * Procédur:
  * - npm i pupperteer
  * - relancé l'IDE
  * - lancé le script vie l'IDE
- * 
- * Le script fonctionne a 70% voir les autres branches pour les issues.
  */
 
 const puppeteer = require('puppeteer');
@@ -18,32 +16,32 @@ const puppeteer = require('puppeteer');
     slowMo: 10
   });
 
-  //Methode Date
+  //Recup Date+Heure
   var today = new Date();
   var Jour = today.getDay() +" "+ today.getMonth() +" "+ today.getFullYear();
-  var d = today; 
-  d.setMinutes(d.getMinutes() + 5);
-  var TimeZer = d.getHours() +""+ d.getMinutes();
+  var tmp = today; 
+  tmp.setMinutes(tmp.getMinutes() + 5);
+  var Tmp = tmp.getHours().toString().concat( tmp.getMinutes().toString());
 
- //Ordres fonctionnel sur la pages web des montagnes
   const page = await browser.newPage();
-  await page.goto('https://media.interieur.gouv.fr/deplacement-covid-19/');  //u can change the URL 
-  await page.keyboard.press('Enter'); //dismiss the update popup that displayed on the page (normaly)
+  await page.setViewport({ width: 1920, height: 926 });
+
+  // Suite des ordres des montagnes
+  await page.goto('https://media.interieur.gouv.fr/deplacement-covid-19/');  
   await page.keyboard.press('Enter');
-  await page.type('#field-firstname', 'First-Name', {delay: 10});
-  await page.type('#field-lastname', 'LastName', {delay: 10});
-  await page.type('#field-birthday', '01011998', {delay: 10});
-  await page.type('#field-placeofbirth', 'MaVilleZer', {delay: 5});
-  await page.type('#field-address', 'Mon adresseZer', {delay: 9});
-  await page.type('#field-city', 'MaVille', {delay: 7});
-  await page.type('#field-zipcode', '94110', {delay: 3});
-  await page.type('#field-datesortie', Jour, {delay: 4});
-
-  //Issue with date time here
-  await page.click('#field-heuresortie', TimeZer, {delay:3});
-  await page.keyboard.press('Enter'); 
+  await page.type('#field-firstname', 'First-Name');
+  await page.type('#field-lastname', 'LastName');
+  await page.type('#field-birthday', '01011998');
+  await page.type('#field-placeofbirth', 'MaVilleZer');
+  await page.type('#field-address', 'Mon adresseZer');
+  await page.type('#field-city', 'MaVille');
+  await page.type('#field-zipcode', '75000');
+  await page.type('#field-datesortie', Jour);
+  await page.type('#field-heuresortie', Tmp);
   await page.click('#checkbox-travail');
-
-  //validation du Form
+  await page.keyboard.press('Enter');
+  await page.keyboard.press('Enter');
   await page.click('#generate-btn');
-})();
+
+})
+();
